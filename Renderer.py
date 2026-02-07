426d005fb89ffbe1c58c777de2e85fc790c647d1
Vo Sy Thai Hao <vosythaihao3326@gmail.com>
2026-02-07 22:28:33 +0700

    Add n3D rendering and normalization

--- Renderer.py
+++ Renderer.py
@@ -1,18 +1,62 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def render_image(axe, path: str):
     image = plt.imread(path)
     axe.imshow(image)
 
+
+# ---------- helpers ----------
+def _normalize2D(p):
+    return p / p[2, 0]
+
+
+def _normalize3D(p):
+    return p / p[3, 0]
+
+
+# ---------- 2D ----------
 def render_point2D(axe, point: np.ndarray, color):
+    point = _normalize2D(point)
+
     x = point[0, 0]
     y = point[1, 0]
+
     color = np.array(color) / 255
-    axe.scatter(x, y, s = 10, color = color)
+    axe.scatter(x, y, s=10, color=color)
+
 
 def render_line2D(axe, p1: np.ndarray, p2: np.ndarray, color):
+    p1 = _normalize2D(p1)
+    p2 = _normalize2D(p2)
+
     x = [p1[0, 0], p2[0, 0]]
     y = [p1[1, 0], p2[1, 0]]
+
     color = np.array(color) / 255
-    axe.plot(x, y, color = color, linewidth = 1)
+    axe.plot(x, y, color=color, linewidth=1)
+
+
+# ---------- 3D ----------
+def render_point3D(axe, point: np.ndarray, color):
+    point = _normalize3D(point)
+
+    x = point[0, 0]
+    y = point[1, 0]
+    z = point[2, 0]
+
+    color = np.array(color) / 255
+    axe.scatter(x, y, z, s=10, color=color)
+
+
+def render_line3D(axe, p1: np.ndarray, p2: np.ndarray, color):
+    p1 = _normalize3D(p1)
+    p2 = _normalize3D(p2)
+
+    x = [p1[0, 0], p2[0, 0]]
+    y = [p1[1, 0], p2[1, 0]]
+    z = [p1[2, 0], p2[2, 0]]
+
+    color = np.array(color) / 255
+    axe.plot(x, y, z, color=color, linewidth=1)
